f192af6d59b5cd77066f096df427f235466d2b62
HITRUST\frankzhang <frankzhang@hitrust.com.tw>
2022-10-03 15:26:40 +0800

    Day 05 - Angular 小學堂（一）

---
+++ MRTStationList/src/app/station-list.const.ts
@@ -0,0 +1,132 @@
+export const stationList = [
+  {
+    "Station": "七張站",
+    "Destination": "新店站",
+    "UpdateTime": "2022-10-03T15:12:04.48"
+  },
+  {
+    "Station": "士林站",
+    "Destination": "象山站",
+    "UpdateTime": "2022-10-03T15:11:53.287"
+  },
+  {
+    "Station": "大坪林站",
+    "Destination": "松山站",
+    "UpdateTime": "2022-10-03T15:12:04.48"
+  },
+  {
+    "Station": "大坪林站",
+    "Destination": "新北產業園區站",
+    "UpdateTime": "2022-10-03T15:12:16"
+  },
+  {
+    "Station": "大直站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2022-10-03T15:12:20"
+  },
+  {
+    "Station": "大直站",
+    "Destination": "動物園站",
+    "UpdateTime": "2022-10-03T15:12:14"
+  },
+  {
+    "Station": "中山國小站",
+    "Destination": "南勢角站",
+    "UpdateTime": "2022-10-03T15:12:21.573"
+  },
+  {
+    "Station": "中正紀念堂站",
+    "Destination": "大安站",
+    "UpdateTime": "2022-10-03T15:12:02.53"
+  },
+  {
+    "Station": "內湖站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2022-10-03T15:12:14"
+  },
+  {
+    "Station": "公館站",
+    "Destination": "新店站",
+    "UpdateTime": "2022-10-03T15:12:04.48"
+  },
+  {
+    "Station": "民權西路站",
+    "Destination": "北投站",
+    "UpdateTime": "2022-10-03T15:12:13.007"
+  },
+  {
+    "Station": "永安市場站",
+    "Destination": "南勢角站",
+    "UpdateTime": "2022-10-03T15:12:16.423"
+  },
+  {
+    "Station": "石牌站",
+    "Destination": "大安站",
+    "UpdateTime": "2022-10-03T15:11:53.287"
+  },
+  {
+    "Station": "江子翠站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2022-10-03T15:12:08.497"
+  },
+  {
+    "Station": "行天宮站",
+    "Destination": "蘆洲站",
+    "UpdateTime": "2022-10-03T15:12:21.573"
+  },
+  {
+    "Station": "忠孝復興站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2022-10-03T15:11:58.21"
+  },
+  {
+    "Station": "忠孝新生站",
+    "Destination": "頂埔站",
+    "UpdateTime": "2022-10-03T15:11:58.21"
+  },
+  {
+    "Station": "東湖站",
+    "Destination": "動物園站",
+    "UpdateTime": "2022-10-03T15:12:14"
+  },
+  {
+    "Station": "松江南京站",
+    "Destination": "松山站",
+    "UpdateTime": "2022-10-03T15:11:54.893"
+  },
+  {
+    "Station": "松江南京站",
+    "Destination": "新店站",
+    "UpdateTime": "2022-10-03T15:11:54.893"
+  },
+  {
+    "Station": "南勢角站",
+    "Destination": "蘆洲站",
+    "UpdateTime": "2022-10-03T15:12:11.23"
+  },
+  {
+    "Station": "科技大樓站",
+    "Destination": "動物園站",
+    "UpdateTime": "2022-10-03T15:12:14"
+  },
+  {
+    "Station": "迴龍站",
+    "Destination": "南勢角站",
+    "UpdateTime": "2022-10-03T15:12:06.387"
+  },
+  {
+    "Station": "唭哩岸站",
+    "Destination": "淡水站",
+    "UpdateTime": "2022-10-03T15:12:13.007"
+  },
+  {
+    "Station": "新埔站",
+    "Destination": "亞東醫院站",
+    "UpdateTime": "2022-10-03T15:12:18.71"
+  },
+  {
+    "Station": "萬芳社區站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2022-10-03T15:12:14"
+  }
+]
