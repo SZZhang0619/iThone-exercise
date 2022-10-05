4d0b9fcdc5ab707c490ab6ce60dc15ada5f9e701
frankzhang <toshibaCEO2015@gmail.com>
2022-10-05 13:44:09 +0800

    Day 20 - 路由（三）

--- RoutingPracticeForAngular30Days/src/app/app-routing.module.ts
+++ RoutingPracticeForAngular30Days/src/app/app-routing.module.ts
@@ -1,20 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path:'',
+        redirectTo:'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: "about",
+        component: AboutComponent
+      },
+    ]
   },
   {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: "about",
-    component: AboutComponent
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '**',
