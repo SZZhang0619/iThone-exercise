1296b3c98283ee7eadae2caeeaf0cf3014ab020d
frankzhang <toshibaCEO2015@gmail.com>
2022-10-06 17:03:02 +0800

    Angular 小學堂（四之一）

---
+++ Angular30DaysECApplication/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerInfoComponent } from './checkout/customer-info/customer-info.component';
+import { PaymentInfoComponent } from './checkout/payment-info/payment-info.component';
+import { ProductItemComponent } from './shared/product-item/product-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerInfoComponent,
+    PaymentInfoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
