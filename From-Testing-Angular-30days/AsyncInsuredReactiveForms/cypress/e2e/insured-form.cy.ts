1e0088a20d43a1c8e85ceb7a33edae0f57013965
Frank Zhang <toshibaCEO2015@gmail.com>
2022-10-13 16:42:49 +0800

    Day21 - E2E 測試實作 - 被保人表單

---
+++ From-Testing-Angular-30days/AsyncInsuredReactiveForms/cypress/e2e/insured-form.cy.ts
@@ -0,0 +1,76 @@
+import insuredForm from '../fixtures/insured-form.json';
+/// <reference types="cypress" />
+
+describe('Insured Form', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+
+  it('have title "Reactive Forms 實作 ─ 被保險人', () => {
+    // Arrange
+    const title = 'Reactive Forms 實作 ─ 被保險人'
+    // Assert
+    cy.get('h1').should('have.text', title);
+  });
+
+  it('should can add the insured', () => {
+    // Arrange
+    const name = insuredForm.name;
+    const gender = insuredForm.gender;
+    const age = insuredForm.age;
+    // Act
+    cy.get('[type="button"]').click();
+    cy.get('#name-0').type(name);
+    cy.get(`[for="${gender}-0"]`).click();
+    cy.get('#age-0').select(age);
+    // Assert
+    cy.get('[type="submit"]').should('be.enabled');
+  });
+
+  it('should can delete the insured', () => {
+    // Act
+    cy.get('[type="button"]').click();
+    cy.get('fieldset').contains('刪除').click();
+    // Assert
+    cy.get('fieldset').should('have.length',0);
+  });
+
+  it('should can not add the insured when the age is not valid', () => {
+    // Arrange
+    const name = insuredForm.name;
+    const gender = insuredForm.gender;
+    // Act
+    cy.get('[type="button"]').click();
+    cy.get('#name-0').type(name);
+    cy.get(`[for="${gender}-0"]`).click();
+    // Assert
+    cy.get('[type="submit"]').should('be.disabled');
+  });
+
+  it('should can not add the insured when the gender is not valid', () => {
+    // Arrange
+    const name = insuredForm.name;
+    const age = insuredForm.age;
+    // Act
+    cy.get('[type="button"]').click();
+    cy.get('#name-0').type(name);
+    cy.get('#age-0').select(age);
+    // Assert
+    cy.get('[type="submit"]').should('be.disabled');
+  });
+
+  it('should can not add the insured when the name is not valid', () => {
+    // Arrange
+    const gender = insuredForm.gender;
+    const age = insuredForm.age;
+    // Act
+    cy.get('[type="button"]').click();
+    cy.get(`[for="${gender}-0"]`).click();
+    cy.get('#age-0').select(age);
+    // Assert
+    cy.get('[type="submit"]').should('be.disabled');
+  });
+
+
+
+})
